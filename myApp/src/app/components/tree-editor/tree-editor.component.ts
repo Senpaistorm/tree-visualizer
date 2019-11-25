449787f1c4a7363066ca194d424ae11fb2d17966
Senpaistorm <austin.l@qq.com>
2019-11-25 12:38:56 -0500

    Added tree editor component

---
+++ myApp/src/app/components/tree-editor/tree-editor.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-editor',
+  templateUrl: './tree-editor.component.html',
+  styleUrls: ['./tree-editor.component.css']
+})
+export class TreeEditorComponent implements OnInit {
+
+  @Input() treeNodes;
+  @Input() treeEdges;
+  nodeSize = 40;
+  height = null;
+  width = null;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.height = document.documentElement.clientHeight;
+    this.width = document.getElementById('tree-editor').offsetWidth;
+  }
+
+  onResize() {
+    this.height = Math.max(this.height, document.documentElement.clientHeight);
+    this.width = Math.max(this.width, document.getElementById('tree-editor').offsetWidth);
+  }
+
+
+}
