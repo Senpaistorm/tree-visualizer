b9713099787fd21ad38792be5f79281ff1116c6d
Senpaistorm <austin.l@qq.com>
2019-11-26 14:49:51 -0500

    Fixed image pathing

--- myApp/src/app/treeOptions.ts
+++ myApp/src/app/treeOptions.ts
@@ -3,19 +3,19 @@ export const treeOptionsData = [
     name: 'Binary Search Tree',
     description: `A binary search tree is a rooted binary tree, whose key in each node must be greater than or equal
     to any key stored in the left sub-tree, and less than or equal to any key stored in the right sub-tree.`,
-    img: '../../assets/Binary_search_tree.svg',
+    img: 'assets/Binary_search_tree.svg',
   },
   {
     name: 'AVL Tree',
     description: `AVL tree is a self-balancing Binary Search Tree (BST) where the
     difference between heights of left and right subtrees cannot be more than one for all nodes.`,
-    img: `../../assets/AVL_Tree.gif`
+    img: `assets/AVL_Tree.gif`
   },
   {
     name: 'Heap',
     description: `In computer science, a heap is a specialized tree-based data structure which is essentially
     an almost complete tree that satisfies the heap property: in a max heap, for any given node C, if P is a
     parent node of C, then the key (the value) of P is greater than or equal to the key of C.`,
-    img: `../../assets/Heap.gif`
+    img: `assets/Heap.gif`
   }
   ];
